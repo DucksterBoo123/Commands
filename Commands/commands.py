483e877c6c6fc11bd6c30557fdd4f411a3f41b22
Duckster Boo <60486814+DucksterBoo123@users.noreply.github.com>
2023-11-08 09:17:04 +0000

    Add files via upload

--- Commands/commands.py
+++ Commands/commands.py
@@ -2,24 +2,27 @@ import os
 
 print("1: yt-dlp")
 print("2: ffmpeg convert")
-
+print("U: update executables")
 menu = input("Select an option: ")
-
-if menu == "1":
+#UPDATER
+if menu == "U":
+    os.system("python3 updater.py")
+    os.system("python3 commands.py")
+#YTDOWNLOADER
+elif menu == "1":
     URL = input("Paste your YT Link: ")
     output = input("Name the file: ")
-
     os.system(""" yt-dlp -f "bestaudio[ext=wav]/bestaudio" "{}" -o "output.%(ext)s" """.format(URL))
     os.system("ffmpeg -i output.webm output.wav")
     os.remove("output.webm")
     os.rename("output.wav", "{}.wav".format(output))
+#FFMPEG
 elif menu == "2":
     print ("Choose your input type:")
     print ("1. Image")
     print ("2. Video")
     print ("3. Audio")
     inptype = input ("...")
-
     if inptype == "1":
         print ("Choose your input format:")
         print ("1. png")
@@ -32,7 +35,6 @@ elif menu == "2":
             option = "jpg"
         if imagetype == "3":
             option = "webp"
-
     if inptype == "2":
         print ("Choose your input format:")
         print ("1. mp4")
@@ -45,7 +47,6 @@ elif menu == "2":
             option = "mkv"
         if imagetype == "3":
             option = "mov"
-    
     if inptype == "3":
         print ("Choose your input format:")
         print ("1. wav")
@@ -58,13 +59,11 @@ elif menu == "2":
             option = "mp3"
         if imagetype == "3":
             option = "flac"
-
     print ("Choose your output type:")
     print ("1. Image")
     print ("2. Video")
     print ("3. Audio")
     inptype = input ("...")
-
     if inptype == "1":
         print ("Choose your output format:")
         print ("1. png")
@@ -77,7 +76,6 @@ elif menu == "2":
             choice = "jpg"
         if imagetype == "3":
             choice = "webp"
-
     if inptype == "2":
         print ("Choose your output format:")
         print ("1. mp4")
@@ -90,7 +88,6 @@ elif menu == "2":
             choice = "mkv"
         if imagetype == "3":
             choice = "mov"
-    
     if inptype == "3":
         print ("Choose your output format:")
         print ("1. wav")
@@ -103,7 +100,10 @@ elif menu == "2":
             choice = "mp3"
         if imagetype == "3":
             choice = "flac"
-
     file1 = "1." + option
     file2 = "2." + choice
-    os.system("ffmpeg -i " + file1 + " " + file2)  
+    os.system("/opt/homebrew/bin/ffmpeg -i " + file1 + " " + file2)
+#Throw and Callback
+else:
+    print("not an option")
+    os.system("python3 commands.py")
